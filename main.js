7e06908bc2e6705fde69816dd6e3536469ad7798
Rakshan <rakshanshetty2003@gmail.com>
2023-07-31 22:27:53 +0530

    saveaLL

---
+++ main.js
@@ -0,0 +1,60 @@
+function calculate_SGPA() {
+    var credit1 = parseFloat(document.getElementById("credit1").value);
+    var mark1 = parseFloat(document.getElementById("marks1").value);
+    var point1 = getPointFromMark(mark1); // Get points based on the marks
+
+    var credit2 = parseFloat(document.getElementById("credit2").value);
+    var mark2 = parseFloat(document.getElementById("marks2").value);
+    var point2 = getPointFromMark(mark2);
+
+    var credit3 = parseFloat(document.getElementById("credit3").value);
+    var mark3 = parseFloat(document.getElementById("marks3").value);
+    var point3 = getPointFromMark(mark3);
+
+    var credit4 = parseFloat(document.getElementById("credit4").value);
+    var mark4 = parseFloat(document.getElementById("marks4").value);
+    var point4 = getPointFromMark(mark4);
+
+    var credit5 = parseFloat(document.getElementById("credit5").value);
+    var mark5 = parseFloat(document.getElementById("marks5").value);
+    var point5 = getPointFromMark(mark5);
+
+    var credit6 = parseFloat(document.getElementById("credit6").value);
+    var mark6 = parseFloat(document.getElementById("marks6").value);
+    var point6 = getPointFromMark(mark6);
+
+    var total_credit = credit1 + credit2 + credit3 + credit4 + credit5 + credit6;
+    var total_points = (credit1 * point1) + (credit2 * point2) + (credit3 * point3) + (credit4 * point4) + (credit5 * point5) + (credit6 * point6);
+
+    // total_credit = total_credit.toFixed(4);
+    // total_points = total_points.toFixed(4);
+
+    var SGPA = (total_points / total_credit).toFixed(2);
+
+    document.getElementById("result").innerText = `Your SGPA is: ${SGPA}`;
+
+    // Clear the input fields without affecting the result paragraph
+    // const form = document.getElementById("sgpaForm");
+    // form.reset();
+
+    // const reset1 = document.getElementById("reset");
+    // reset1.reset();
+}
+
+function getPointFromMark(mark) {
+    if (mark >= 90) {
+        return 10;
+    } else if (mark >= 80) {
+        return 9;
+    } else if (mark >= 70) {
+        return 8;
+    } else if (mark >= 60) {
+        return 7;
+    } else if (mark >= 50) {
+        return 6;
+    } else if (mark >= 40) {
+        return 5;
+    } else {
+        return 0;
+    }
+}
